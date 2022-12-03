20a4c4480a6385be12d263900edbf39f57563b83
yantias <yantiamaliasari@gmail.com>
2022-12-04 00:30:16 +0700

    perubahan main activity

--- app/src/main/java/com/example/jastipchingu/MainActivity.java
+++ app/src/main/java/com/example/jastipchingu/MainActivity.java
@@ -2,30 +2,31 @@ package com.example.jastipchingu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    actionBar actionBar:
+    ImageView imageButton5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageButton5=(ImageView)
+            findViewById(R.id.imageButton5);
+        imageButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                startActivity(intent);
 
-        actionBar - getSupportActionBar():
-        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+            }
+        });
 
-    }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return true;
-    }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
     }
 }
